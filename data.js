06df54284892369f76659fcebb03c1e1697e0e59
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2021-09-29 22:56:13 -0700

    Initial Commit

---
+++ data.js
@@ -0,0 +1,17 @@
+const data = [{
+    "address": "1 W Century Dr #38A, Los Angeles, CA 90067",
+    "price": "$21,000,000",
+    "name": "The Cavalli Penthouse",
+    "bedrooms": 2,
+    "bathrooms": 5,
+    "sqft": "5,254",
+    "type": "Condo",
+    "parking": 1,
+    "built": "2010",
+    "hoa": "$5,372",
+    "ppsqft": "$3,997",
+    "flooring": "hardwood",
+    "furnished": "yes"
+}]
+
+export default data;
